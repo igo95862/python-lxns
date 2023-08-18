275c4afd04b7fe362bf9befa42f56c6b3e5e994f
igo95862 <igo95862@yandex.ru>
2023-08-18 20:52:10 +0600

    Make namespace file descriptor handling more strict
    Emit a warning when namespace was not closed.
    
    Add a close() method that closes the file descriptor. Can be called
    on already closed namespace without error just like file objects.
    
    Add context manager methods. Namespace objects can be used with
    the `with` statements. Namespace will be closed after exiting
    `with` block.

--- src/lxns/namespace.py
+++ src/lxns/namespace.py
@@ -6,6 +6,7 @@ from os import O_CLOEXEC, O_RDONLY
 from os import close as close_fd
 from os import open as open_fd
 from typing import TYPE_CHECKING
+from warnings import warn
 
 from .os import CLONE_NEWUSER, ns_get_userns, setns
 
@@ -16,21 +17,40 @@ if TYPE_CHECKING:
 class BaseNamespace:
     NAMESPACE_CONSTANT: ClassVar[int] = -1
     NAMESPACE_PROC_NAME: ClassVar[str] = ""
-    _fd: int
+    _fd: int | None
 
     def __init__(self, *args: Any, **kwargs: Any):
         raise NotImplementedError(
-            "Please use one of the 'from' methods to initialize namespace"
+            "Please use one of the 'from' methods to initialize namespace."
         )
 
     def __del__(self) -> None:
-        close_fd(self._fd)
+        if self._fd is not None:
+            warn(f"unclosed namespace {self}", ResourceWarning)
+            self.close()
 
     def setns(self) -> None:
+        if self._fd is None:
+            raise ValueError("Trying switch to closed namespace.")
+
         setns(self._fd, self.NAMESPACE_CONSTANT)
 
     def get_userns(self) -> UserNamespace:
+        if self._fd is None:
+            raise ValueError("Namespace closed. Cannot get user namespace.")
+
         return UserNamespace(ns_get_userns(self._fd))
+
+    def close(self) -> None:
+        if self._fd is not None:
+            close_fd(self._fd)
+            self._fd = None
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(self, *args: Any, **kwargs: Any) -> None:
+        self.close()
 
     @classmethod
     def from_pid(cls, pid: int | Literal["self"]) -> Self:
