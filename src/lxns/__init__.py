e2aae58d3b06a114270bbd4e18556ff3b3a92571
igo95862 <igo95862@yandex.ru>
2024-03-24 21:01:52 +0500

    Rename the namespace.py to namespaces.py to match upstream docs
    All documentation refers to namespaces. Also remove the imports
    from root `lxns` module. All imports should be done using submodules
    like `lxns.namespaces`.

--- src/lxns/__init__.py
+++ src/lxns/__init__.py
@@ -1,7 +1,3 @@
 # SPDX-License-Identifier: MPL-2.0
 # SPDX-FileCopyrightText: 2023 igo95862
 from __future__ import annotations
-
-from .namespace import UserNamespace
-
-__all__ = ("UserNamespace",)
